9a2b186ebfabed066c2d17a2d8b70509d2e97984
Nicholas Guzman <nguzman23193@columbushs.com>
2021-01-26 13:03:41 -0500

    finished

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const correctAnswers =['B', 'B', 'B', 'B'];
+const correctAnswers = ['B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
-    e.preventDefault();
+  e.preventDefault();
 
-    let score = 0;
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+  let score = 0;
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
-    //check answers
-    userAnswers.forEach((answer, index) => {
-        if(answer === correctAnswers[index]){
-            score += 25;
-        }
-    });
+  // check the answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]){
+      score += 25;
+    }
+  });
 
-    console.log(score);
-})
+  // show the result
+  result.querySelector('span').textContent = `${score}%`;
+  result.classList.remove('d-none');
+
+});
