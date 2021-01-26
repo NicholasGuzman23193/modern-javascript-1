1d83abfc6d8835435258e3dfcf2eb803c45b6b1f
Nicholas Guzman <nguzman23193@columbushs.com>
2021-01-26 13:11:00 -0500

    finished

--- app.js
+++ app.js
@@ -16,7 +16,23 @@ form.addEventListener('submit', e => {
   });
 
   // show the result
+  scrollTo(0,0);
   result.querySelector('span').textContent = `${score}%`;
   result.classList.remove('d-none');
 
 });
+
+//window object
+
+//console.log('hello');
+//window.console.log('hello');
+//console.log(document.querySelector('form'));
+//console.log(window.document.querySelector('form'));
+
+//alert('hello');
+//window.alert('hello');
+
+//setTimeout(()=>{
+//alert('hello, ninjas')
+//}, 3000);
+
